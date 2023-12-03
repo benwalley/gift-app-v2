85acc29e28bfcbc21b4db18e486f83fb683c59cf
Ben Walley <ben.walley@classyllama.com>
2023-12-03 15:17:31 -0600

    get files

--- src/components/AddItemFromShare/index.js
+++ src/components/AddItemFromShare/index.js
@@ -1,24 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-
+import initAmazonPostWorker from "../../workers/getDataFromUrl";
+import getDataFromHtmlString from "../../helpers/getDataFromHtmlString";
+import WebWorker from "../../workers/workerSetup";
+import getData from "../../workers/getDataFromUrl";
 
 
 export default function AddItemFromShare() {
     const [isLoading, setIsLoading] = useState(false);
     const [content, setContent] = useState('')
-    let data = useParams();
+    const [getDataWorker, setGetDataWorker] = useState();
+    const [data, setData] = useState({})
 
     useEffect(() => {
-        setIsLoading(true)
-        const url = new URL(document.location);
-        setContent(url)
-    }, []);
+        // create web worker
+        const newGetDataWorker = new WebWorker(getData)
+        setGetDataWorker(newGetDataWorker);
 
+        newGetDataWorker.addEventListener('message', function(e) {
+            setData(e.data);
+        })
+    }, []);
 
     return (
         <div>
-            {JSON.stringify(data)};
-            {JSON.stringify(content)};
+            {JSON.stringify(data)}
         </div>
     );
 }
--- src/workers/getDataFromUrl.js
+++ src/workers/getDataFromUrl.js
@@ -1,15 +1,38 @@
 
-export default () => {
-    onmessage = async (e) => {
-        try {
-            const url = e.data
-            const resp = await fetch(url);
-            const body = await resp.json();
-            postMessage(body)
-        } catch(e) {
-            console.log(e)
-            postMessage('error')
+export default function initAmazonPostWorker() {
+    this.addEventListener("fetch", (event) => {
+        // Only use this event listener for POST requests sent to /share-file-handler.
+        const url = new URL(event.request.url);
+        if (
+            event.request.method !== "POST" ||
+            url.pathname !== "/create-wishlist-item-post"
+        ) {
+            return;
         }
 
-    }
+        function sendFilesToFrontend(textFiles, htmlFiles, imageFiles) {
+            postMessage({textFiles, htmlFiles, imageFiles})
+        }
+
+        event.respondWith(
+            (async () => {
+                // Get the data from the submitted form.
+                const formData = await event.request.formData();
+
+                // Get the submitted files.
+                const textFiles = formData.getAll("textFiles");
+                const htmlFiles = formData.getAll("htmlFiles");
+                const imageFiles = formData.getAll("images");
+
+                // Send the files to the frontend app.
+                sendFilesToFrontend(textFiles, htmlFiles, imageFiles);
+
+                // Redirect the user to a URL that shows the imported files.
+                // return Response.redirect("/create-wishlist-item", 303);
+            })(),
+        );
+    });
 };
+
+
+
